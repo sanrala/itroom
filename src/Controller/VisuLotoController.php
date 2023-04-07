a44a64be3140b3088c03004c00d042ab75a52da9
sanrala <sebastien.mauro@gmail.com>
2023-04-07 18:27:10 +0200

    first commit

--- src/Controller/VisuLotoController.php
+++ src/Controller/VisuLotoController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use League\Csv\Reader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class VisuLotoController extends AbstractController
 {
@@ -14,5 +17,18 @@ class VisuLotoController extends AbstractController
         return $this->render('visu_loto/index.html.twig', [
             'controller_name' => 'VisuLotoController',
         ]);
+    }
+
+    public function importLoto(SymfonyStyle $io): void
+    {
+        $io->title('Importation des villes');
+    }
+
+    private function readCsvFile(): Reader 
+    {
+        $csv = Reader::createFromPath('%kernel.root.dir%/../import/loto.csv', 'r');
+        $csv->setHeaderOffset(0);
+
+        return $csv;
     }
 }
---
+++ src/Entity/Results.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResultsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResultsRepository::class)]
+class Results
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $resultat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResultat(): ?string
+    {
+        return $this->resultat;
+    }
+
+    public function setResultat(string $resultat): self
+    {
+        $this->resultat = $resultat;
+
+        return $this;
+    }
+}
